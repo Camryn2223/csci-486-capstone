3c74310d7cb5c87c3a4d8ec88a9df10689f74829
camryn2223 <camrynchamberlin@gmail.com>
2026-03-06 20:43:28 -0500

    organizations pivot database table columns added

--- database/migrations/2026_03_06_223812_create_organization_user_table.php
+++ database/migrations/2026_03_06_223812_create_organization_user_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('organization_user', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('organization_id')->constrained('organizations')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->boolean('can_create_job_positions')->default(false);
             $table->timestamps();
         });
     }
