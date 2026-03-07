91f8564c958266a9fb40e3634ac684583dccdd61
camryn2223 <camrynchamberlin@gmail.com>
2026-03-06 20:01:42 -0500

    job position table columns setup

--- database/migrations/2026_03_06_223704_create_job_positions_table.php
+++ database/migrations/2026_03_06_223704_create_job_positions_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('job_positions', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('organization_id')->constrained('organizations')->onDelete('cascade');
+            $table->foreignId('created_by')->constrained('users')->onDelete('cascade');
+            $table->string('title');
+            $table->string('description');
+            $table->string('requirements');
+            $table->enum('status', ['open', 'closed'])->default('open');
             $table->timestamps();
         });
     }
