3fcf41fae1df76a205da76606ae30288b7032f0f
camryn2223 <camrynchamberlin@gmail.com>
2026-04-21 20:18:22 -0400

    fix: enhance storage directory creation in start.sh for better organization

--- docker/start.sh
+++ docker/start.sh
@@ -13,7 +13,12 @@ if ! grep -q '^APP_KEY=.\+' /var/www/html/.env; then
     php artisan key:generate
 fi
 
-mkdir -p /var/www/html/storage /var/www/html/bootstrap/cache
+mkdir -p \
+    /var/www/html/storage/framework/cache/data \
+    /var/www/html/storage/framework/sessions \
+    /var/www/html/storage/framework/views \
+    /var/www/html/storage/logs \
+    /var/www/html/bootstrap/cache
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 chmod -R 777 /var/www/html/storage /var/www/html/bootstrap/cache
 
